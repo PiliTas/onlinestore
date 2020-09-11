bf17c8ee84afaeb58c371bf801cdb33b8a96efc5
NPGonzalez <naypili@gmail.com>
2020-09-11 16:04:33 -0300

    Se conecta a base de datos y se realiza primer query; se obteniene como respuesta, todos los elementos de la tabla product

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 const http = require('http');
+const mysql = require('mysql');
+// SE CONECTA LA BASE DE DATOS
+const db = mysql.createConnection({
+    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+    user: 'bsale_test',
+    password: 'bsale_test',
+    database: 'bsale_test'
+});
+
+db.connect((error) => {
+    if (!!error) {
+        console.log('Error');
+    } else {
+        console.log('Connnected');
+    }
+});
+
+let sql = 'SELECT * FROM product';
+db.query(sql, (error, res, fields) => {
+    if (error) throw error;
+    db.end();
+    console.log(res);
+});
+
+
 
 http.createServer((req, res) => {
-    res.write('Hola mundo')
-}).listen(8080, () => {
-    console.log('Listening port:', 8080);
-});
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); // Se escribirá una página web
+        res.write('Hola mundo'); //Se escribe en el browser
+        res.end(); // Se termina de crear respuesta
+    })
+    .listen(8080, () => {
+        console.log('Listening port:', 8080);
+    });
